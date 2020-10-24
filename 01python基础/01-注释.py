c4d7627f047743878751c8585552691e9e9a8ca2
chengjing <chengjing002@163.com>
2020-10-24 11:04:00 +0800

    初始化项目

---
+++ 01python基础/01-注释.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-10-24 11:02
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 01-注释.py
+# @Software: PyCharm
+
+# 单行注释
+
+"""
+    多行注释
+"""
+
+'''
+    多行注释
+'''
+
+print("python 注释")
