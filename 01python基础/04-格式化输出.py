144cda135715880145e73c65bd9f55f827f1ecaa
chengjing <chengjing002@163.com>
2020-10-24 12:56:03 +0800

    转移字符

--- 01python基础/04-格式化输出.py
+++ 01python基础/04-格式化输出.py
@@ -15,4 +15,9 @@ age = 12
 weight = 89.1
 id = 231
 
+# %s 格式化字符串
 print("我的名字是%s, 今年%d岁，体重%0.2f斤，学号是%05d" % (name, age, weight, id))
+
+# f{} 格式化字符串
+print(f"我的名字是{name}, 今年{age}岁，体重{weight}斤，学号是{id}")
+
---
+++ 01python基础/05-转义字符.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-10-24 12:39
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 05-转义字符.py
+# @Software: PyCharm
+
+"""
+    \n -- 换行
+    \t -- 制表符
+"""
+
+# 结束符
+print("输出的内容", end='\n')
+print("hello")
+print("world")
+print("hello python")
+print("hello\npython")
