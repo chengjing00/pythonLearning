532f1c93e1bd07235c9498cad6224b5f1ab77b95
chengjing <chengjing002@163.com>
2020-10-24 11:24:24 +0800

    定义变量

---
+++ 01python基础/02-变量.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-10-24 11:06
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 02-变量.py
+# @Software: PyCharm
+
+# 定义变量 (数字、字母、下划线组成， 不能以数字开头， 不能使用内置关键字)
+
+
+"""
+1. 定义变量
+    语法： 变量名 = 值
+
+2. 使用变量
+
+3. 看变量特点
+"""
+
+# 定义变量：存储数据Tom
+my_name = "Tom"
+print(my_name)
+
+# 定义变量： 存储数据 黑马程序员
+schoolName = "黑马程序员, I Love python"
+print(schoolName)
