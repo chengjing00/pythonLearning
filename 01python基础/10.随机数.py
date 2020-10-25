39b3e3d3c9f96c7643e4d7ae4d010db25275b02b
chengjing <chengjing002@163.com>
2020-10-25 10:50:57 +0800

    随机数

---
+++ 01python基础/10.随机数.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-10-25 10:47
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 10.随机数.py
+# @Software: PyCharm
+"""
+1. 导入模块
+    import random
+2. 调用方法
+    random.randint(1,4) --> 包含开始，不包含结束
+"""
+
+import random
+
+randomNum = random.randint(1, 400)
+print(randomNum)
