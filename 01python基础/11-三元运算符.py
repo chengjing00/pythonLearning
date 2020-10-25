f096d224be9322d14455d4b4bcab17b3b1b3fc1d
chengjing <chengjing002@163.com>
2020-10-25 11:03:27 +0800

    三目运算符

---
+++ 01python基础/11-三元运算符.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-10-25 11:00
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 11-三元运算符.py
+# @Software: PyCharm
+
+a = 1
+b = 2
+# 三目运算符
+c = a if a > b else b
+print(f"c:{c}")
