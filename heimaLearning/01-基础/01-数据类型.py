c1494b06f23a6396a53a9be709ecec1ea8124a51
chengjing <chengjing002@163.com>
2021-02-20 20:17:13 +0800

    基本数据类型

---
+++ heimaLearning/01-基础/01-数据类型.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/20 下午7:25
+# @Author  : chengj-f
+# @Email   : chengj-f@glodon.com
+# @File    : 01-数据类型.py
+# @Software: PyCharm
+
+
+# int type
+num1 = 1
+# float
+num2 = 1.1
+print(type(num1))
+print(type(num2))
+
+# bool
+b = True
+print(type(b))
+
+# str
+str = "hello world"
+print(type(str))
+
+# list -- 列表
+c = [10, 20, 30]
+print(type(c))
+
+# tuple -- 元组
+d = (10, 20, 30)
+print(type(d))
+
+# set -- 集合
+s = {10, 20, 30}
+print(type(s))
+
+# dict -- 字典 -- 键值对
+f = {"name": "tom", "age": 10}
+print(type(f))
+
+
+
+
+
+
